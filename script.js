a1a30d40eef2beb75d8cd6313fd987117917d90c
Konrad Szymanski <46305261+konradszymanski@users.noreply.github.com>
2020-11-11 15:51:15 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1431,15 +1431,12 @@ document.querySelector("#btn106").addEventListener("click", () =>{
 })
 //107
 //find the number of sorted pairs formed by its elements of a given array of integers such that one element in the pair is divisible by the other one.
-const in107num = document.getElementById("in107num").value;
- const answer107 = document.getElementById("answer107"); 
- const arr107 = in107num.split("").map(Number);
-    const new107 = [];
+  const in107num = document.getElementById("in107num").value;
+  const answer107 = document.getElementById("answer107"); 
+  const arr107 = in107num.split("").map(Number);
+  const new107 = [];
 document.querySelector("#btn107").addEventListener("click", () =>{
-
-  
-    new107.push(arr107)
-
+  new107.push(arr107)
   answer107.textContent =  `- ${new107}`
 })
   // [2, 4, 6] -> 2 - (2,4), (2,6)
