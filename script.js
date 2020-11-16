b4ced563848e378b54cfe1e8fba3b48491e1e3fe
Konrad Szymanski <46305261+konradszymanski@users.noreply.github.com>
2020-11-16 15:26:27 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1478,16 +1478,12 @@ document.querySelector("#btn109").addEventListener("click", () =>{
   const answer109 = document.getElementById("answer109");
 
   for (let i= 1; i <= prime; i++){
-    
     let primeNumbers =[];
-
     primeNumbers.push(i)
-
     answer109.textContent += primeNumbers.filter(numbers => 
       numbers % 3!=0 && 
       numbers % 2!=0 &&
       numbers % 5!=0
       )
-  
-        }
+  }
 });
