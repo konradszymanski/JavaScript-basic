0bc0f1a999336697843c5bf859798494b3df988c
Konrad Szymanski <46305261+konradszymanski@users.noreply.github.com>
2020-12-15 12:15:11 +0000

    117 start

--- script.js
+++ script.js
@@ -1631,3 +1631,7 @@ document.querySelector("#btn116").addEventListener("click", () =>{
     }
   }
 })
+//117 check whether a given matrix is an identity matrix.
+//Note: In linear algebra, the identity matrix, or sometimes ambiguously called a unit matrix, of size n is the n ? n square matrix with ones on the main diagonal and zeros elsewhere.
+//[[1, 0, 0], [0, 1, 0], [0, 0, 1]] -> true
+//[[1, 0, 0], [0, 1, 0], [1, 0, 1]] -> false
